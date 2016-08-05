4d30f743dc44d3e0d5297935bc4bcc1cc564bc40
김태우 [인턴] <kimsin@ftt.inc>
2016-08-05 19:55:53 +0900

    Console message Edit..

--- ChatServer/ServerMain.cs
+++ ChatServer/ServerMain.cs
@@ -10,38 +10,25 @@ namespace ChatServer
         static void Main(string[] args)
         {
 
-            try
+            if (args.Length == 1 && args[0] == "-help")
             {
-                Console.Title = "Chat Server " + Int32.Parse(args[0]);
-                Console.ForegroundColor = ConsoleColor.Green;
-                if (args.Length == 1 && args[0] == "-help")
-                {
-                    Console.WriteLine("listeningPort backEndIp backEndPort maxClientNum");
-                    return;
-                }
+                Console.WriteLine("ChatServer listeningPort backEndIp backEndPort maxClientNum");
+                return;
+            }
 
-                if (args.Length == 0)
-                {
-                    Console.WriteLine("listeningPort backEndIp backEndPort maxClientNum");
-                    return;
-                }
+            if (args.Length == 0)
+            {
+                Console.WriteLine("ChatServer listeningPort backEndIp backEndPort maxClientNum");
+                return;
+            }
 
-                Thread exitInputThread = new Thread(new ThreadStart(ExitInput));
-                exitInputThread.Start();
-                Console.WriteLine("To Exit, Press ESCAPE.");
-                Console.WriteLine("In other way, there's no prove for complete exit and sending FIN");
-                Server server = new Server(Int32.Parse(args[0]), args[1], Int32.Parse(args[2]), Int32.Parse(args[3]));
-                server.Start();
-                server.ShutDown();
-            }
-            catch(Exception e)
-            {
-                Environment.Exit(0);
-            }
-            finally
-            {
-                Environment.Exit(0);
-            }
+            Thread exitInputThread = new Thread(new ThreadStart(ExitInput));
+            exitInputThread.Start();
+            Console.WriteLine("To Exit, Press ESCAPE.");
+            Console.WriteLine("In other way, there's no prove for complete exit and sending FIN");
+            Server server = new Server(Int32.Parse(args[0]), args[1], Int32.Parse(args[2]), Int32.Parse(args[3]));
+            server.Start();
+            server.ShutDown();
         }
 
         private static void ExitInput()
